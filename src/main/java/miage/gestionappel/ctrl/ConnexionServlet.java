04dcb378282a498dd83b927ded00502ed5ec90ca
btissamnachit <btissam.nachit21@gmail.com>
2022-02-07 22:40:38 +0100

    [1]- Se deconnecter

--- src/main/java/miage/gestionappel/ctrl/ConnexionServlet.java
+++ src/main/java/miage/gestionappel/ctrl/ConnexionServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
-
+@WebServlet(name = "ConnexionServlet", value = "/connexion")
 public class ConnexionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ src/main/java/miage/gestionappel/ctrl/DeconnexionServlet.java
@@ -0,0 +1,24 @@
+package miage.gestionappel.ctrl;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "DeconnexionServlet", value = "/deconnexion")
+public class DeconnexionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        session.removeAttribute("nom");
+        session.removeAttribute("prenom");
+        session.removeAttribute("email");
+        session.removeAttribute("role");
+        request.getRequestDispatcher("login").forward(request,response);
+    }
+}
