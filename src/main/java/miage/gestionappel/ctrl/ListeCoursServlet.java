02609f7e4c566a9912b107348599a318302f69e5
RadoANDRIA <rado.andriamampianina@gmail.com>
2022-02-09 11:09:55 +0100

    [5]Consulter la liste d'étudiant d'un cours

--- src/main/java/miage/gestionappel/ctrl/ListeCoursServlet.java
+++ src/main/java/miage/gestionappel/ctrl/ListeCoursServlet.java
@@ -1,21 +1,15 @@
 package miage.gestionappel.ctrl;
-
-
-import miage.gestionappel.dao.GroupeDao;
 import miage.gestionappel.dao.ProfesseurDao;
 import miage.gestionappel.metier.Cours;
 import miage.gestionappel.metier.Professeur;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.*;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Set;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-@WebServlet(name = "ListeCoursServlet", value = "/ListeCoursServlet")
+import javax.servlet.http.HttpSession;
+import java.io.PrintWriter;
 
 public class ListeCoursServlet extends HttpServlet {
 
@@ -23,30 +17,28 @@ public class ListeCoursServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
             ServletException, IOException {
-        /*----- Récupérer les informations -----*/
-        String email = request.getParameter("email");
+
+        HttpSession session = request.getSession(true);
+        String email = (String) session.getAttribute("email");
+        request.getParameter("email");
 
         ProfesseurDao professeurDao = new ProfesseurDao();
-        Professeur professeur = new Professeur();
+        Professeur professeur = professeurDao.getByEmail(email);
 
-        professeur = professeurDao.getByEmail(email);
+        Set<Cours> cours = professeur.getCours();
+        request.setAttribute("cours", cours);
 
-//        Cours cours =
-//        Set<Cours> cours = professeur.getCours();
-//
-//            for (Cours c : cours) {
-//                System.out.println("résultat : " + c.getNomC());
-//            }
-//
-//            request.setAttribute("cours", cours);
-//            request.getRequestDispatcher("/listeetudiants").forward(request, response);
-//
-//
-}
+        for (Cours c : cours) {
+            System.out.println("résultat : " + c.getNomC());
+        }
+        request.getRequestDispatcher("/listeetudiants").forward(request, response);
+        }
 
         @Override
-        protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        protected void doPost (HttpServletRequest request, HttpServletResponse response) throws
+        ServletException, IOException {
 
         }
     }
+
 
